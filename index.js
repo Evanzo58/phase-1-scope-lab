c9350f8f007286b5160cd1354ee8949a9254e269
evans <cheruiyote405@gmail.com>
2025-03-24 13:37:58 +0300

    completing labs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+var bestCustomer;
+let leastFavoriteCustomer = "someone"; 
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new person";
+}
